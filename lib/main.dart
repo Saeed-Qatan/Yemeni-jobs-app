8e10bfce4536e24e31fcb554d4421e97f9273365
unknown <saeedqat541@gmail.com>
2025-11-01 22:26:27 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:yemeni_jops/view/SplashScreen.dart';
+import 'package:yemeni_jops/view/login_page.dart';
 
-void main() {
   void main() {
     runApp(Directionality(textDirection: TextDirection.rtl, child: MyApp()));
   }
-}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Yemeni Job Search',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Cairo'),
-      home: const SplashScreen(),
+      home: const LoginPage(),
     );
   }
 }
